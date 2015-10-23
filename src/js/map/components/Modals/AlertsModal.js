4cb970e874b1e238244042c12e8d96e2f70ca53b
Albert Marquez <amarquez@blueraster.com>
2015-10-23 15:19:27 -0400

    Began skeleton for alerts subscription form Added Formsy dependency

--- src/js/map/components/Modals/AlertsModal.js
+++ src/js/map/components/Modals/AlertsModal.js
@@ -1,25 +1,34 @@
 import {modalText} from 'js/config';
 import ModalWrapper from 'components/Modals/ModalWrapper';
+import {EmailInput} from 'components/Forms/EmailInput';
 import React from 'react';
+import {Form} from 'formsy-react';
 
 export default class AlertModal extends React.Component {
 
-  constructor(props) {
+  getInitialState () {
+    return {
+      validationErrors: {}
+    }
+  }
+
+  constructor (props) {
     super(props);
+    this.state = this.getInitialState()
   }
 
   render() {
     return (
       <ModalWrapper>
         <div>{modalText.alerts.title}</div>
-        <div>{modalText.alerts.descriptions.email}</div>
-        <div>
-          <input type='text' placeholder='Name'></input>
-        </div>
-        <div>{modalText.alerts.descriptions.phone}</div>
-        <div>
-          <input type='text' placeholder='Phone'></input>
-        </div>
+        <Form>
+          <EmailInput className='hidden' name='email' validations='isEmail' validationErrors={{isEmail: 'Please enter a valid email'}}>example@email.com</EmailInput>
+          <div>
+            <div>
+              <button>Subscribe</button>
+            </div>
+          </div>
+        </Form>
       </ModalWrapper>
     );
   }
