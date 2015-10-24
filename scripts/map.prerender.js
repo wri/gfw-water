842204a12eb103fc003f83403625d1496a42a430
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-10-24 16:50:09 -0400

    added files

---
+++ scripts/map.prerender.js
@@ -0,0 +1,61 @@
+var ReactDomServer = require('react-dom/server');
+var moduleMapper = require('./moduleMapper');
+var requirejs = require('requirejs');
+var cheerio = require('cheerio');
+var React = require('react');
+var path = require('path');
+var fs = require('fs');
+
+//- Root path of project
+var basepath = path.join(__dirname, '../');
+
+//- Paths for the components, modules root, build profile, and dist html file
+var paths = {
+  canopyModal: 'js/components/Modals/CanopyModal',
+  alertsModal: 'js/components/Modals/AlertsModal',
+  layerModal: 'js/components/Modals/LayerModal',
+  map: 'js/components/Map',
+  modulesRoot: path.join(basepath, 'build/js/map'),
+  profile: path.join(basepath, 'map.build.js'),
+  html: path.join(basepath, 'dist/map.html')
+};
+
+//- Read in the profile and convert to JSON
+var buildProfile = fs.readFileSync(paths.profile, 'utf-8');
+var buildConfig = eval(buildProfile);
+
+//- Remap missing modules to a module with no dependencies to prevent requirejs from trying to load them
+//- module mapper will return an object like below for all dojo/esri modules used in the project
+//- { 'esri/map': 'js/config', 'dojo/dom-class': 'js/config' }
+var moduleMap = moduleMapper(paths.modulesRoot, 'js/config');
+
+requirejs.config({
+  baseUrl: path.join(basepath, buildConfig.baseUrl),
+  paths: buildConfig.paths,
+  map: { '*': moduleMap },
+  nodeRequire: require
+});
+
+//- Get Components, make them a factory, and render them to static markup
+var CanopyModal = React.createFactory(requirejs(paths.canopyModal));
+var AlertsModal = React.createFactory(requirejs(paths.alertsModal));
+var LayerModal = React.createFactory(requirejs(paths.layerModal));
+var Map = React.createFactory(requirejs(paths.map));
+
+var canopyModal = ReactDomServer.renderToStaticMarkup(CanopyModal());
+var alertModal = ReactDomServer.renderToStaticMarkup(AlertsModal());
+var layerModal = ReactDomServer.renderToStaticMarkup(LayerModal());
+var map = ReactDomServer.renderToStaticMarkup(Map());
+
+//- Load in the Distribution HTML and append all the components into it
+var $ = cheerio.load(fs.readFileSync(paths.html, 'utf-8'));
+$('#canopy-modal').append(canopyModal);
+$('#alerts-modal').append(alertModal);
+$('#layer-modal').append(layerModal);
+$('#root').append(map);
+
+//- Write out the distribution html file
+fs.writeFile(paths.html, $.html(), function (writeErr) {
+  if (writeErr) { console.error(writeErr); return; }
+  console.log('Components successfully prerendered.');
+});
---
+++ scripts/moduleMapper.js
@@ -0,0 +1,28 @@
+/**
+* Create a map object for requirejs to remap dojo and esri modules to a dependency free module
+* @param {string} filepath - Filepath to start mapping dependencies from
+* @param {string} remapPath - path to remap dojo and esri modules to
+* @return {object} map
+* - ex. { path: remapPath, path1: remapPath }
+*/
+module.exports = function (filepath, remapPath) {
+  var madge = require('madge');
+
+  var dependencies = madge(filepath, { format: 'amd' }).tree,
+      modulePaths = [],
+      map = {};
+
+  var filterFunction = function filterFunction (modulePath) {
+    return (modulePath.search('esri') > -1) || (modulePath.search('dojo') > -1);
+  };
+
+  for (var moduleName in dependencies) {
+    modulePaths = modulePaths.concat(dependencies[moduleName].filter(filterFunction));
+  }
+
+  modulePaths.forEach(function (path) {
+    map[path] = remapPath;
+  });
+
+  return map;
+};
