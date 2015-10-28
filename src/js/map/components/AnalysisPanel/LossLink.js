5e98be6e2f7eb8886ac5a9b480f8d150a9517f32
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-10-28 10:35:42 -0400

    added to new files for the analysis panel

---
+++ src/js/map/components/AnalysisPanel/LossLink.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class LossLink extends React.Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <span>
+
+      </span>
+    );
+  }
+
+}
