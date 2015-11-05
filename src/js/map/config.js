aa1ebe7183ee9686e43f37f9b9a047fd265a1180
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-11-05 13:09:26 -0500

    added new params for hydrology request

--- src/js/map/config.js
+++ src/js/map/config.js
@@ -254,9 +254,10 @@ export const config = {
       url: 'http://utility.arcgis.com/usrsvcs/appservices/epPvpBkwsBSgIYCd/rest/services/Tools/Hydrology/GPServer/Watershed',
       params: {
         f: 'json',
-        Generalize: true,
-        SnapDistance: 5000,
-        SnapDistanceUnits: 'Meters'
+        Generalize: 'True',
+        SnapDistance: '5000',
+        SnapDistanceUnits: 'Meters',
+        SourceDatabase: 'FINEST'
       },
       outputSR: 102100,
       jobId: 'WatershedArea'
