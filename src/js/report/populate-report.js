4922255ef308c1b8866d704ba3d12227b956b40d
swingley <swingley@gmail.com>
2016-01-12 17:22:41 -0800

    Find and replace 'potential' with 'past' and fixed issue where plan for action was showing the wrong actions based on risks specific to a watershed.

--- src/js/report/populate-report.js
+++ src/js/report/populate-report.js
@@ -47,7 +47,7 @@ export default {
     const treePercent = treeCover / wsArea;
     const dams = attributes.dams_c;
 
-    const potentialCover = attributes.ptc_ha / 1000000;
+    const pastCover = attributes.ptc_ha / 1000000;
     const waterWithdrawl = attributes.wd_c;
 
     const treeLossRisk = attributes.rs_tl_c;
@@ -56,10 +56,10 @@ export default {
     const treeLossRate = attributes['tlt_g' + canopy + '_ha'];
     const treeLossTrend = (treeLossRate > -1) ? 'positive' : 'negative';
 
-    const potentialLossRisk = attributes.rs_pf_c;
-    const potentialLossAmount = attributes.ptc_ha / 1000000;
-    const potentialLossPercent = potentialLossAmount / wsArea;
-    const potentialLossRate = attributes['tl_g' + canopy + '_all_ha'] / attributes.ptc_ha;
+    const pastLossRisk = attributes.rs_pf_c;
+    const pastLossAmount = attributes.ptc_ha / 1000000;
+    const pastLossPercent = pastLossAmount / wsArea;
+    const pastLossRate = attributes['tl_g' + canopy + '_all_ha'] / attributes.ptc_ha;
 
     const erosionRisk = attributes.rs_sed_c;
     const erosionRiskDescription = riskCategory(erosionRisk);
@@ -75,7 +75,7 @@ export default {
     dom.byId('tree-cover-percent').innerHTML = number.format(treePercent, percentTwoDecimals);
     dom.byId('dam-count').innerHTML = dams;
 
-    dom.byId('potential-cover').innerHTML = number.format(potentialCover, oneDecimal);
+    dom.byId('past-cover').innerHTML = number.format(pastCover, oneDecimal);
     dom.byId('water-withdrawl').innerHTML = waterWithdrawl;
 
     dom.byId('risk-tree-loss').innerHTML = treeLossRisk;
@@ -84,10 +84,10 @@ export default {
     dom.byId('tree-loss-rate').innerHTML = treeLossRate;
     dom.byId('tree-loss-trend').innerHTML = treeLossTrend;
 
-    dom.byId('risk-potential-loss').innerHTML = potentialLossRisk;
-    dom.byId('potential-loss-amount').innerHTML = number.format(potentialLossAmount, oneDecimal);
-    dom.byId('potential-loss-percent').innerHTML = number.format(potentialLossPercent, percent);
-    dom.byId('potential-loss-rate').innerHTML = number.format(potentialLossRate, percent);
+    dom.byId('risk-past-loss').innerHTML = pastLossRisk;
+    dom.byId('past-loss-amount').innerHTML = number.format(pastLossAmount, oneDecimal);
+    dom.byId('past-loss-percent').innerHTML = number.format(pastLossPercent, percent);
+    dom.byId('past-loss-rate').innerHTML = number.format(pastLossRate, percent);
 
     dom.byId('risk-erosion').innerHTML = erosionRisk;
     dom.byId('risk-erosion-description').innerHTML = erosionRiskDescription;
@@ -105,13 +105,13 @@ export default {
     // Figure out which risk rows to show in Plan for Action section.
     const visibleClassName = 'applicable';
     const noRisk = domQuery('tr.no-risk')[0];
-    if (treeLossRisk > 3) {
+    if (pastLossRisk > 3) {
       console.log('treeLossRisk over 3');
       domClass.add(domQuery('tr.tree-loss-risk')[0], visibleClassName);
     }
-    if (potentialLossRisk > 3) {
-      console.log('potentialLossRisk over 3');
-      domClass.add(domQuery('tr.potential-loss-risk')[0], visibleClassName);
+    if (treeLossRisk > 3) {
+      console.log('pastLossRisk over 3');
+      domClass.add(domQuery('tr.past-loss-risk')[0], visibleClassName);
     }
     if (erosionRisk > 3) {
       console.log('erosionRisk over 3');
@@ -121,7 +121,7 @@ export default {
       console.log('fireRisk over 3');
       domClass.add(domQuery('tr.fire-risk')[0], visibleClassName);
     }
-    if (treeLossRisk > 3 || potentialLossRisk > 3 || erosionRisk > 3 || fireRisk > 3) {
+    if (treeLossRisk > 3 || pastLossRisk > 3 || erosionRisk > 3 || fireRisk > 3) {
       domClass.add(noRisk, 'risk-info');
     }
   }
