464ed9435de02591a92b80dc64be07b83362448c
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-07-11 12:01:12 -0400

    Testing some reg expressions in htaccess that will detect browser and redirect

--- locals.js
+++ locals.js
@@ -94,9 +94,9 @@ module.exports = {
     title: 'Browser not supported | Global Forest Watch Water',
     message: 'This website is optimized for these browsers. Please upgrade to a supported browser and try loading the website again.',
     browsers: [
-      {label: 'Chrome', href: 'https://www.google.com/intl/en/chrome/browser/desktop/index.html'},
-      {label: 'Firefox', href: 'https://www.mozilla.org/en-US/firefox/all/'},
-      {label: 'Safari', href: 'http://www.apple.com/safari/'},
+      {label: 'Chrome > 49', href: 'https://www.google.com/intl/en/chrome/browser/desktop/index.html'},
+      {label: 'Firefox > 45', href: 'https://www.mozilla.org/en-US/firefox/all/'},
+      {label: 'Safari > 7', href: 'http://www.apple.com/safari/'},
       {label: 'Internet Explorer 10', href: 'https://support.microsoft.com/en-us/help/18520/download-internet-explorer-11-offline-installer'}
     ]
   }
