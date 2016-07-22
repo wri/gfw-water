45bc25562bec14bae663a87de8ea3103fd26767c
Lucas Cotner <lc07@uw.edu>
2016-07-22 12:52:53 -0400

    Updated report content, styles, and legends

--- src/map/js/components/LayerPanel/ReportLegend.js
+++ src/map/js/components/LayerPanel/ReportLegend.js
@@ -4,7 +4,9 @@ export default class ReportLegend extends React.Component {
   render() {
     return (
       <div className='report-legend-inner'>
-        <div className='report-legend-label'>{this.props.title}</div>
+        {this.props.title === 'Historical Forest Loss' ? null :
+          <div className='report-legend-label'>{this.props.title}</div>
+        }
         {this.props.title === 'Recent Forest Loss' ?
         <span dangerouslySetInnerHTML={{__html: '<svg width="10" height="10"><rect width="10" height="10" style="fill:rgb(255,96,151);"></svg>'}} />
         : null}
