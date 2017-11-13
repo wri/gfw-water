2cbd214581d4b8884bc2162c26e314c48c7d1265
Lucas Cotner <lc07@uw.edu>
2017-11-13 17:14:03 -0500

    Header via nightly assets directly

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ var gulp = require('gulp');
 
 //- Set the version number to match package.json
 var isProd = process.env.HEADER_ENV === 'production';
-locals.headerScript = isProd ? locals.headerScriptProd : locals.headerScriptStage;
-console.log(process.env.NODE_ENV);
+// locals.headerScript = isProd ? locals.headerScriptProd : locals.headerScriptStage;
+locals.headerScript = locals.headerScriptStage;
+console.log(isProd);
 console.log(locals.headerScript);
 var version = require('./package.json').version;
 locals.version = version;
